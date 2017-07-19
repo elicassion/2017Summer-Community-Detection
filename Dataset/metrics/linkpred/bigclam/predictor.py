592363d10fc1c29b9a6f842d1ccbec93a39b21dd
elicassion <elicassion@sjtu.edu.cn>
2017-07-19 19:17:58 +0800

    support bigclam

--- Dataset/metrics/linkpred/bigclam/predictor.py
+++ Dataset/metrics/linkpred/bigclam/predictor.py
@@ -13,17 +13,36 @@ class predictor(object):
 
     def load_data(self, data_dir):
         self.uname2uid = {}
-        for line in open(os.path.join(data_dir, 'network_snap.txt')):
-            line = line.split()
+        for line in open(os.path.join(data_dir, 'link.txt')):
+            line = line.split('\t')
             if line[0] not in self.uname2uid:
                 self.uname2uid[line[0]] = len(self.uname2uid)
             if line[1] not in self.uname2uid:
                 self.uname2uid[line[1]] = len(self.uname2uid)
+        print ("load data done.")
 
     def load_result(self, result_prefix, uname2uid):
-        self.f = np.loadtxt(result_prefix + '.F.txt')  # U*C
-        self.U = self.f.shape[0]
-        self.C = self.f.shape[1]
+        resf = open(os.path.join(result_prefix, n + '.f.txt'), 'r')
+        res = resf.read()
+        # print (res)
+        lines = res.split('\n')
+        au_num = len(uname2uid)
+        self.f = np.zeros([au_num, cc])
+        for au_id, line in enumerate(lines[1:-1]):
+            items = line.split('\t')
+            au = uname2uid[items[0]]
+            edges = items[1].strip(' ')
+            if edges is '0':
+                continue
+            edges = edges.split(' ')
+            for i in range(1, len(edges)-1, 2):
+                comm_id = int(edges[i])
+                self.f[au_id][comm_id] = float(edges[i+1])
+        self.U = au_num
+        self.C = cc
+        print ("load result done.")
+        # print (np.dot(self.f[0], self.f[1]))
+        print (np.dot(norm.pdf(1994, self.mu[0], self.sigma[0]), norm.pdf(1998, self.mu[1], self.sigma[1])))
 
     def link_predict(self, from_user, to_user):
         result = np.dot(self.f[from_user], self.f[to_user])
