69c079b13e0684ac9f0ca502e03dc0b34a534e99
elicassion <elicassion@sjtu.edu.cn>
2017-07-19 18:35:17 +0800

    modify output csv

--- Dataset/metrics/linkpred/predict.py
+++ Dataset/metrics/linkpred/predict.py
@@ -27,8 +27,8 @@ if __name__ == '__main__':
     versions = {
         'cdot': [
             'CDOT2',
-            # 'CDOT3',
-            # 'CDOT8'
+            'CDOT3',
+            'CDOT8'
         ]
     }
     dataset_path = 'data'
@@ -42,15 +42,16 @@ if __name__ == '__main__':
                 if versions[model]:
                     for version in versions[model]:
                         subprocess.run('python predict_batch.py {0:s} {1:s} {2:s} {3:s} {4:s}'.format(dataset_path, model, mode, conference, version), shell=True, stdout=sys.stdout, stderr=subprocess.STDOUT)
+                        result += pickle.load(open('result_batch.pkl', 'rb'))
                 else:
                     subprocess.run('python predict_batch.py {0:s} {1:s} {2:s} {3:s} {4:s}'.format(dataset_path, model, mode, conference, ''), shell=True, stdout=sys.stdout, stderr=subprocess.STDOUT)
-                result += pickle.load(open('result_batch.pkl', 'rb'))
+                    result += pickle.load(open('result_batch.pkl', 'rb'))
                 pickle.dump(result, open('result.pkl', 'wb'))   
     for i in result:
         for k in data.keys():
             data[k].append(i[k])
     df = pd.DataFrame(data=data)
-    df = df.ix[df.groupby(['model', 'mode', 'conference'])['score'].idxmax().values, :].reset_index(drop=True)
+    df = df.ix[df.groupby(['model', 'mode', 'conference', 'version'])['score'].idxmax().values, :].reset_index(drop=True)
     out_filename = os.path.join(root, 'measure', expi, 'link_pred_{date:s}.csv'.format(date=str(datetime.datetime.now()).replace(":", "-")))
     if not os.path.exists(os.path.dirname(out_filename)):
         os.makedirs(os.path.dirname(out_filename))
