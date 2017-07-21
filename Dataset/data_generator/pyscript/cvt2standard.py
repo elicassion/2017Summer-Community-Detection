0e7e58d3c81d07727a90f70a90a5820f01a58147
elicassion <elicassion@sjtu.edu.cn>
2017-07-21 17:17:12 +0800

    cvt .comm file ok

--- Dataset/data_generator/pyscript/cvt2standard.py
+++ Dataset/data_generator/pyscript/cvt2standard.py
@@ -15,7 +15,7 @@ baseyear = 1989
 for prefix in prefixs:
 	# comms_years derive format:
 	# year comm_number
-	# comm comm comm...
+	# comm_id \t au au au...
 	# ...
 	# \n
 	# year comm_number
@@ -23,26 +23,42 @@ for prefix in prefixs:
 	# au_comm_years derive format:
 	# au \t comm,year,year,year,... comm,year,year,year,...
 	# ...
-	comms_years = []
+	# comm_au_years = []
+	au_comm_years = []
+	cvt_file_dir = os.path.join(cvt_data_path, prefix)
+	if not os.path.exists(cvt_file_dir):
+		os.mkdirs(cvt_file_dir)
+	comm_au_years_file = open(os.path.join(cvt_file_dir, "comm_au.txt"), "w")
+	au_comm_years_file = open(os.path.join(cvt_file_dir, "au_comm.txt"), "w")
 	for t_step in range(1, t_steps+1):
 		comm_filename = os.path.join(gen_data_path, "%s.t%02d.comm" % (prefix, t_step))
 		edges_filename = os.path.join(gen_data_path, "%s.t%02d.edges" % (prefix, t_step))
-		cvt_file_dir = os.path.join(cvt_data_path, prefix)
-		if not os.path.exists(cvt_file_dir):
-			os.mkdirs(cvt_file_dir)
-		node_set = set()
-		nodes = []
-		edges = []
+
+		# convert comms
+
+		# comm_au = []
+		au_comm = {}
 		cf = open(comm_filename, "r")
-		maxn = 0
-		for cid, line in enumerate(cf.read().split('\n')[:-1]):
+		lines = cf.read().split('\n')[:-1]
+		comm_au_years_file.write("%d %d\n" % (baseyear+t_step, len(lines)))
+		for cid, line in enumerate(lines):
 			members = line.split(' ')[:-1]
-			for mem in members:
-				if int(mem) not in node_set:
-					node_set.add(int(mem))
-					
-			
+			comm_au_years_file.write("%d\t" % cid)
+			for mem in members[:-1]:
+				comm_au_years_file.write("%s " % mem)
+				if mem not in au_comm.keys():
+					au_comm[mem] = {}
+				if cid not in au_comm[mem].keys():
+					au_comm[mem][cid] = set()
+				au_comm[mem][cid].add(baseyear+t_step)
+			comm_au_years_file.write("%s\n" % members[-1])
+		comm_au_years_file.write("\n")
+		au_comm_years.append(au_comm)
 		cf.close()
+
+
+		# convert links
+		edges = []
 		ef = open(edges_filename, "r")
 		for link in ef.read().split('\n')[:-1]:
 			tpl = link.split(' ')
