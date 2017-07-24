24c73401f12bb14a3bd97052f812f30f815a28db
elicassion <elicassion@sjtu.edu.cn>
2017-07-24 10:27:27 +0800

    collecting t2 information while loading data instead of while generating pos edges

--- Dataset/metrics/timepred/cdot/predictor.py
+++ Dataset/metrics/timepred/cdot/predictor.py
@@ -2,6 +2,7 @@ import numpy as np
 import os
 from math import exp
 from scipy.stats import norm
+from sklearn.preprocessing import MinMaxScaler as mmnorm
 
 class predictor(object):
 
@@ -27,6 +28,7 @@ class predictor(object):
                 t1 = int(tp[0])
                 t2 = int(tp[1])
                 uvt1 = (doc_id, ref_id, t1)
+                # Currently ignore the condition when it has multiple t2
                 if uvt1 not in self.uvt1_rec.keys():
                     self.uvt1_rec[uvt1] = set()
                 self.uvt1_rec[uvt1].add(t2)
@@ -66,13 +68,27 @@ class predictor(object):
         # print (np.dot(self.f[0], self.f[1]))
         print (np.dot(norm.pdf(1994, self.mu[0], self.sigma[0]), norm.pdf(1998, self.mu[1], self.sigma[1])))
 
-    def link_predict(self, from_user, to_user, from_time, to_time, toleration):
-        # TODO: revise formula
-        result = 0
-        for i in range(self.f.shape[1]):
-            result += self.f[from_user][i] * self.f[to_user][i] * \
-                    norm.pdf(from_time, self.mu[from_user][i], self.sigma[from_user][i]) * \
-                    norm.pdf(to_time, self.mu[to_user][i], self.sigma[to_user][i])
-        # print (result)
-        result = 1 - exp(-result)
-        return result
+    def time_predict(self, from_user, to_user, from_time, to_time, toleration):
+        # TODO: mode
+        time_pred_mode = 'topk'
+        uvt1 = (from_user, to_user, from_time)
+        true_t2 = self.uvt1_rec[uvt1]
+        predict_p = {}
+        for t in range(1984, 2017):
+            result = 0
+            for i in range(self.f.shape[1]):
+                result += self.f[from_user][i] * self.f[to_user][i] * \
+                        norm.pdf(from_time, self.mu[from_user][i], self.sigma[from_user][i]) * \
+                        norm.pdf(t, self.mu[to_user][i], self.sigma[to_user][i])
+            result = 1 - exp(-result)
+            p[t] = result
+        st_p = sorted(adict.items(), key = lambda item:item[1], reverse = True)
+        st_p_keys = [item[0] for item in st_p]
+        st_p_values = [item[1] for item in st_p]
+        # min max norm
+        st_p_values = mmnorm.fit_transform(np.array(st_p_values)).tolist()
+        accumulate_p = 0
+        for t2 in true_t2:
+            accumulate_p += st_p_values[st_p_keys.index(t2)]
+        accumulate_p = accumulate_p / len(true_t2)
+        return accumulate_p
