c3c74a6c7eb10a5cd6752b0d26a69a0e50111891
elicassion <elicassion@sjtu.edu.cn>
2017-08-30 09:56:43 +0800

    analysis person

--- Dataset/data_analysis/calc_cc_time.py
+++ Dataset/data_analysis/calc_cc_time.py
@@ -1,5 +1,6 @@
 import numpy as np
 import os
+import math
 from math import log, exp
 from scipy.stats import norm
 from sklearn.preprocessing import MinMaxScaler
@@ -7,42 +8,41 @@ import sys
 
 class Predictor(object):
 
-    def __init__(self, data_dir, result_dir):
+    def __init__(self, data_dir, result_dir, cc):
         super(predictor, self).__init__()
-        # self.data_dir = data_dir
-        # self.result_prefix = result_prefix
-        # self.load()
+        self.data_dir = data_dir
+        self.result_dir = result_dir
+        self.cc = cc
         self.load_data(data_dir)
         self.load_result(result_dir)
+        self.uname2uid = {}
+        self.links = set()
+
 
     def load_data(self, data_dir):
-        self.uname2uid = {}
+        
         for line in open(os.path.join(data_dir, 'links.txt')):
             line = line.strip().split('\t')
             if line[0] not in self.uname2uid:
                 self.uname2uid[line[0]] = len(self.uname2uid)
             if line[1] not in self.uname2uid:
                 self.uname2uid[line[1]] = len(self.uname2uid)
-        for line in open(os.path.join(data_dir, 'del_links.txt')):
-            line = line.strip().split('\t')
-            if line[0] not in self.uname2uid:
-                self.uname2uid[line[0]] = len(self.uname2uid)
-            if line[1] not in self.uname2uid:
-                self.uname2uid[line[1]] = len(self.uname2uid)
-        print ("load data done.")
+        # for line in open(os.path.join(data_dir, 'del_links.txt')):
+        #     line = line.strip().split('\t')
+        #     if line[0] not in self.uname2uid:
+        #         self.uname2uid[line[0]] = len(self.uname2uid)
+        #     if line[1] not in self.uname2uid:
+        #         self.uname2uid[line[1]] = len(self.uname2uid)
+        print ("Load Data Done.")
 
-    def load_result(self, result_prefix, n, uname2uid, cc):
-        # print (os.path.join(result_prefix, n + '.f_mu_sigma.txt'))
-        resf = open(os.path.join(result_prefix, n + '.f_mu_sigma.txt'), 'r')
+    def load_result(self, result_dir):
+        resf = open(os.path.join(result_dir, 'final.f_mu_sigma.txt'), 'r')
         res = resf.read()
-        # print (res)
         lines = res.split('\n')
-        au_num = len(uname2uid)
-        # L0=19
-        self.f = np.zeros([au_num, cc])
-        # self.mu = np.zeros([au_num, cc])
-        self.mu = np.linspace(2008, 2008, au_num*cc).reshape((au_num, cc))
-        self.sigma = np.ones([au_num, cc])
+        au_num = len(self.uname2uid)
+        self.f = np.zeros([au_num, self.cc])
+        self.mu = np.linspace(2008, 2008, au_num*self.cc).reshape((au_num, self.cc))
+        self.sigma = np.ones([au_num, self.cc])
         for au_id, line in enumerate(lines[1:-1]):
             items = line.split('\t')
             au = items[0]
@@ -58,28 +58,11 @@ class Predictor(object):
                 self.f[au_id][comm_id] = float(tpl[1])
                 self.mu[au_id][comm_id] = float(tpl[2])
                 self.sigma[au_id][comm_id] = float(tpl[3])
-                # print (tpl)
         self.U = au_num
-        self.C = cc
-        print ("load result done.")
+        self.C = self.cc
+        print ("Load Result Done.")
         # print (np.dot(self.f[0], self.f[1]))
-        print (np.dot(norm.pdf(1994, self.mu[0], self.sigma[0]), norm.pdf(1998, self.mu[1], self.sigma[1])))
-
-
-    def calc_error(self, x, y):
-        return abs(x-y)/(2016-1980)
-
-    def calc_min_error(self, p_t2, true_t2, used_true_t2, toleration):
-        min_e = 99999
-        min_e_t2 = None
-        for t2 in true_t2.keys():
-            if t2 in used_true_t2:
-                continue
-            calc_e = self.calc_error(p_t2, t2)
-            if calc_e < min_e:
-                min_e = calc_e
-                min_e_t2 = t2
-        return min_e, min_e_t2
+        # print (np.dot(norm.pdf(1994, self.mu[0], self.sigma[0]), norm.pdf(1998, self.mu[1], self.sigma[1])))
 
     def time_predict(self, from_user, to_user, from_time, to_time, predict_mode, toleration):
         # TODO: mode
@@ -87,7 +70,6 @@ class Predictor(object):
         # direct: use toleration to compare
         # maybe else~
 
-        
         if predict_mode == 'nlog':
             nlog = np.sum(self.f[from_user]*self.f[to_user]*\
                             norm.pdf(from_time, self.mu[from_user], self.sigma[from_user])*\
@@ -147,6 +129,13 @@ class Predictor(object):
                 result = result / sum_freq
                 return result
 
-data_dir = os.path.join('..', 'data', 't_cite', 'AAAI')
-result_dir = os.path.join('res', 'AAAI')
-predictor = Predictor(data_dir, result_dir, 287)
+    def show_distribute(self, uname):
+        uid = uname2uid[uname]
+        print (self.f[uid])
+        print (self.mu[uid])
+        print (self.sigma[uid])
+
+data_dir = os.path.join('..', 'data', 'test_fos', 'big_data')
+result_dir = os.path.join('..', 'res', 'cdot', 'test_fos', 'big_data', 'bd_082800')
+predictor = Predictor(data_dir, result_dir, 34)
+distribute = predictor.show_distribute('61F1FBBD')
